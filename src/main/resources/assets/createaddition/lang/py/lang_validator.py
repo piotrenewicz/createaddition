3a936ab634497371f1ae84c8f3b302f1797e924c
ArrivedBog593 <MarioRomeroCastro@hotmail.com>
2025-04-10 14:16:38 -0700

    Refactor lang_validator.py to dynamically load language files and improve directory handling

--- src/main/resources/assets/createaddition/lang/py/lang_validator.py
+++ src/main/resources/assets/createaddition/lang/py/lang_validator.py
@@ -1,35 +1,24 @@
 import json
 import os
 
-dir = os.path.dirname(__file__)
+dir_actual = os.path.dirname(__file__)
+dir_lang = os.path.join(dir_actual, "../")
 
 truth = "en_us.json"
-langs = [
-    "sv_se.json",
-    "uk_ua.json",
-    "zh_cn.json",
-    "de_de.json",
-    "es_es.json",
-    "es_mx.json",
-    "ja_jp.json",
-    "ko_kr.json",
-    "pt_br.json",
-    "ru_ru.json"
-]
 
-with open(dir + "/../" + truth, 'r', encoding="utf8") as truthFile:
-    truthJson = json.loads(truthFile.read())
+langs = [f for f in os.listdir(dir_lang) if f.endswith(".json") and f != truth]
+
+with open(os.path.join(dir_lang, truth), 'r', encoding="utf8") as truthFile:
+    truthJson = json.load(truthFile)
     for lang in langs:
         newLang = {}
-        with open(dir + "/../" + lang, 'r', encoding="utf8") as currentFile:
-            langJson = json.loads(currentFile.read())
-            for (key, truthValue) in truthJson.items():
+        with open(os.path.join(dir_lang, lang), 'r', encoding="utf8") as currentFile:
+            langJson = json.load(currentFile)
+            for key, truthValue in truthJson.items():
                 newLang[key] = truthValue
-                 if key in langJson:
+                if key in langJson:
                     newLang[key] = langJson[key]
                 else:
                     print("Key: " + key + " not in lang")
-                    
-        with open(dir + "/../" + lang, 'w', encoding="utf8") as currentFile:
+        with open(os.path.join(dir_lang, lang), 'w', encoding="utf8") as currentFile:
             currentFile.write(json.dumps(newLang, ensure_ascii=False, indent="\t"))
-
